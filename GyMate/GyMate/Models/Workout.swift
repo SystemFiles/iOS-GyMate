4ffcdbe00c9ab9f71ee9ed17e000e55f4dff61a0
Liam Stickney <liam.0820@hotmail.com>
2019-11-14 14:57:03 -0500

    Added the Workout Object
    Added the workout object to the models folder.

---
+++ GyMate/GyMate/Models/Workout.swift
@@ -0,0 +1,13 @@
+//
+//  Workout.swift
+//  GyMate
+//
+//  Created by Liam Stickney on 2019-11-14.
+//  Copyright © 2019 The Burrito Boys. All rights reserved.
+//
+
+import UIKit
+
+class Workout: NSObject {
+
+}
