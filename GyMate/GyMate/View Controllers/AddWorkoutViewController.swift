386fbd7cd8609219a619a9f503f52be866ad01cc
Liam Stickney <liam.0820@hotmail.com>
2019-11-21 10:45:08 -0500

    Completed Dashboard UI

---
+++ GyMate/GyMate/View Controllers/AddWorkoutViewController.swift
@@ -0,0 +1,20 @@
+//
+//  AddWorkoutViewController.swift
+//  GyMate
+//
+//  Created by Liam Stickney on 2019-11-21.
+//  Copyright © 2019 The Burrito Boys. All rights reserved.
+//
+
+import UIKit
+
+class AddWorkoutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
---
+++ GyMate/GyMate/View Controllers/DashboardViewController.swift
@@ -0,0 +1,23 @@
+//
+//  DashboardViewController.swift
+//  GyMate
+//
+//  Created by Liam Stickney on 2019-11-21.
+//  Copyright © 2019 The Burrito Boys. All rights reserved.
+//
+
+import UIKit
+
+class DashboardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func rewindToDashboardVC(sender: UIStoryboardSegue!) {
+        // DO NOTHING
+    }
+
+}
